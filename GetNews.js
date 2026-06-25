17c3fda9ddaaab34290ef0f5cbc7ea81d7467588
tosa <111439988+SveaGUN@users.noreply.github.com>
2026-06-25 12:18:38 +0900

    add indexページの追加

---
+++ GetNews.js
@@ -0,0 +1,14 @@
+﻿async function loadArticles() {
+    const response = await fetch("https://xenospectrum.com/api/v1/entities/company/nvidia");
+    const json = await response.json();
+    const articles = json.data.relatedArticles;
+    let html = '';
+
+    const ul = document.getElementById("ul_news");
+    html = articles.map(article => `
+        <li>
+        <a href="${article.url}" target="_blank">${article.title}</a>
+        <span>${new Date(article.publishedAt).toLocaleDateString("ja-JP")}</span>
+        </li>`).join("");
+    ul.innerHTML = html;
+}
